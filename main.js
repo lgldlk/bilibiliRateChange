0913aa0695cacca22f23e11bb7133a0f4d6276ac
lgldlk <57523724+lgldlk@users.noreply.github.com>
2021-06-15 20:33:03 +0800

    Update main.js
    版本更新，处理b站bwp-video的问题

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         bilibili多倍速调节（支持剧集和视频）
 // @namespace    lgldlk
-// @version      0.1
-// @description  bilibili多倍速调节（支持剧集和视频）
+// @version      0.3
+// @description  bilibili多倍速调节（支持剧集和视频）~~~
 // @author       lgldlk
 // @include      *://*.bilibili.com/video/*
 // @include      *://*.bilibili.tv/video/*
@@ -24,10 +24,10 @@ function waitForNode(nodeSelector, callback) {
     }
 }
 
+
 function debounce(func, wait) {
     let timer;
     return function() {
-        let context = this;
         let args = arguments;
         if (timer) clearTimeout(timer);
         timer = setTimeout(() => {
@@ -51,17 +51,31 @@ function setCacheRate(video, rate) {
 function setRateText(rate) {
     document.querySelector(".bilibili-player-video-btn-speed-name").innerText = `${rate}x`;
 }
+
+function detection() {
+    let rateButtons = document.querySelector(".bilibili-player-video-btn-speed-menu-list");
+    if (rateButtons.innerHTML != "5x") {
+        initRateBody(null);
+    }
+
+}
 const initRateBody = function(callBack) {
     waitForNode(() => document.querySelector('div.bilibili-player-video-btn-speed > div > ul'),
         (node) => {
             var oV = document.getElementsByTagName("video")[0];
+            if (oV == undefined) {
+                alert("清空缓存后刷新即可使用")
+                return;
+            }
             deleteChild(node)
             for (let i of rateArr) {
 
                 var tmpLi = document.createElement('li');
                 tmpLi.classList = "bilibili-player-video-btn-speed-menu-list";
                 tmpLi.innerText = `${ i}x`;
-
+                tmpLi.style.height = "30px"
+                tmpLi.style["font-size"] = "16px"
+                tmpLi.style["line-height"] = "30px"
                 tmpLi.addEventListener("click", function(k) {
                     return function() {
                         cacheRate = k
@@ -71,10 +85,10 @@ const initRateBody = function(callBack) {
                 }(i));
                 node.appendChild(tmpLi);
             }
+            setTimeout(detection, 1000)
             oV.addEventListener('DOMNodeRemoved', () => {
                 if (cacheFlag == true) {
                     let tmp = debounce(function() {
-                        console.log("initRateBody(setCacheRate);cacheFlag=false;}")
                         initRateBody(setCacheRate(oV, cacheRate));
                         setRateText(cacheRate)
                         cacheFlag = true;
